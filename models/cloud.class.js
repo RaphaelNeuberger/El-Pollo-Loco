05708f73ec33484bc291cc24f541e665180e1c42
Raphael Neuberger <70220250+RaphaelNeuberger@users.noreply.github.com>
2026-01-22 09:38:15 +0100

    SJump optimise

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,7 +1,7 @@
 class Cloud extends MovableObject {
   y = 20;
   height = 250;
-  widght = 500;
+  width = 500;
 
   constructor() {
     super().loadImage("img/5_background/layers/4_clouds/1.png");
